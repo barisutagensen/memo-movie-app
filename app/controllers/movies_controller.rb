1ec2e620b216368d735a9c61adac2e52727330c1
barisutagensen <f.kawano08@gmail.com>
2022-03-17 17:21:01 +0900

    コントローラー　投稿機能アクション記述

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,18 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
   
+  def new
+    @movies = Movie.new
+  end
+
+  def create
+    Movie.create(movie_params)
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :image, :memo, :actor, :director, :genre_id, :evaluation_id).merge(user_id: current_user.id)
+  end
+
+
 end
