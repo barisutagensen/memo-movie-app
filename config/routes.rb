18cc12f8ead6c41073a5eed97fda0f6867244e2a
barisutagensen <f.kawano08@gmail.com>
2022-04-09 23:24:13 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "movies#index"
-  resources :movies
+  resources :movies do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: :show
 end
